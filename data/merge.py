7f05a537e7b667d5775d2b9763734a2776ef7730
zengyao <zengyao@tencent.com>
2019-10-29 15:59:54 +0800

    add data augment and polydecay

---
+++ data/merge.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import csv
+import sys
+
+lines = []
+with open("./deliver_qa.out.fix.tsv", "r", encoding="utf-8-sig") as f:
+    reader = csv.reader(f, delimiter="\t", quotechar=None)
+    for i,line in enumerate(reader):
+        if(i==0):
+            continue
+        example = []
+        if sys.version_info[0] == 2:
+            line = list(unicode(cell, 'utf-8') for cell in line)
+        example.append(line[3])
+        example.append(line[4])
+        example.append(line[5])
+        lines.append(example)
+with open("./badcase.to20190711.txt", "r", encoding="utf-8-sig") as f:
+    reader = csv.reader(f, delimiter="\t", quotechar=None)
+    for i, line in enumerate(reader):
+        if (i == 0):
+            continue
+        example=[]
+        if sys.version_info[0] == 2:
+            line = list(unicode(cell, 'utf-8') for cell in line)
+        example.append(line[0])
+        example.append(line[1])
+        example.append(line[2])
+        lines.append(example)
+df=pd.DataFrame(lines,columns=list("ABC"))
+print(len(df))
+print(df)
+X_train, X_test=train_test_split(df,test_size=0.2,stratify=df['C'],shuffle=True)
+X_train.to_csv("train.csv",header=True,index=False)
+X_test.to_csv("dev.csv",header=True,index=False)
+print(X_train["A"])
+print(X_train)
+print(len(X_train))
+print(len(X_train[X_train["C"]=="0"])/len(X_train[X_train["C"]=="1"]))
--- optimization.py
+++ optimization.py
@@ -59,7 +59,21 @@ class WarmupLinearSchedule(LambdaLR):
         if step < self.warmup_steps:
             return float(step) / float(max(1, self.warmup_steps))
         return max(0.0, float(self.t_total - step) / float(max(1.0, self.t_total - self.warmup_steps)))
+class WarmupPolynomialSchedule(LambdaLR):
+    """ Linear warmup and then linear decay.
+        Linearly increases learning rate from 0 to 1 over `warmup_steps` training steps.
+        Linearly decreases learning rate from 1. to 0. over remaining `t_total - warmup_steps` steps.
+    """
+    def __init__(self, optimizer, warmup_steps, t_total, power=2, last_epoch=-1):
+        self.warmup_steps = warmup_steps
+        self.t_total = t_total
+        self.power=power
+        super(WarmupPolynomialSchedule, self).__init__(optimizer, self.lr_lambda, last_epoch=last_epoch)
 
+    def lr_lambda(self, step):
+        if step < self.warmup_steps:
+            return float(step) / float(max(1, self.warmup_steps))
+        return max(0.0, float(self.t_total - step) / float(max(1.0, self.t_total - self.warmup_steps))**self.power)
 
 class WarmupCosineSchedule(LambdaLR):
     """ Linear warmup and then cosine decay.
